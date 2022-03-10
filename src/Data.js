976c232e00e350790061c610979b66f33d151eef
locz <loc.6.10.20000@gmail.com>
2022-03-10 14:50:08 +0700

    first commit

---
+++ src/Data.js
@@ -0,0 +1,11 @@
+
+const datas = [
+    {name: 'Chu Duc Loc' ,age: 21, id: 1},
+    {name: 'Hoang Khang Anh' ,age: 20, id: 2},
+    {name: 'Tran Van Tuan' ,age: 22, id: 3},
+    {name: 'Le Viet Hung' ,age: 24, id: 4},
+    {name: 'Nguyen Duc Chinh' ,age: 26, id: 5},
+    {name: 'Pham Xuan Hien' ,age: 25, id: 6}
+]
+
+export default datas;
